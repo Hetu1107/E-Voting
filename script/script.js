49655f933a94919b0833d9cdbcef9cbf6a257ada
Hetu Patel <hetu200211@gmail.com>
2021-07-08 14:23:29 +0530

    script changed

--- script/script.js
+++ script/script.js
@@ -8,9 +8,11 @@ function join_voting(){
     host.style.display = "none";
     join.style.display = "block";
 }
-
+var a =[];
+var g;
 function addFields(){
     var number = document.getElementById("member").value;
+    g = number;
     var container = document.getElementById("container");
     while (container.hasChildNodes()) {
         container.removeChild(container.lastChild);
@@ -18,6 +20,8 @@ function addFields(){
     for (i=0;i<number;i++){
         container.appendChild(document.createTextNode("Option " + (i+1)));
         var input = document.createElement("input");
+        var k = i.toString();
+        input.setAttribute("id",k);
         input.type = "text";
         input.name = "member" + i;
         input.placeholder = "enter name...."
@@ -26,4 +30,14 @@ function addFields(){
         // Append a line break 
         container.appendChild(document.createElement("br"));
     }
+}
+function get(){
+    for(i=0;i<g;i++)
+    {
+        var l = i.toString();
+        var h = document.getElementById(l).value;
+        a.push(h);
+        console.log(a[i]);
+    }
+    
 }
